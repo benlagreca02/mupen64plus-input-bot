36f9a11296c4340706e343809e58743d88a1b93a
Benjamin LaGreca <benlagreca02@gmail.com>
2024-01-01 14:07:49 -0500

    Updated version number only, no code changes

--- src/version.h
+++ src/version.h
@@ -3,7 +3,7 @@
 
 #define PLUGIN_NAME    "Mupen64Plus Bot Input Plugin"
 #define PLUGIN_VERSION           0x000001
-#define INPUT_PLUGIN_API_VERSION 0x020000
+#define INPUT_PLUGIN_API_VERSION 0x020100
 
 #define VERSION_PRINTF_SPLIT(x) (((x) >> 16) & 0xffff), (((x) >> 8) & 0xff), ((x) & 0xff)
 
